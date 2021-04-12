114ca967155a8663cd3697054387c5aef91f9bc1
soyal <xiongxiao9958@gmail.com>
2021-04-12 23:11:17 +0800

    函数重构

--- lesson6/6.4.cpp
+++ lesson6/6.4.cpp
@@ -10,32 +10,48 @@ using std::vector;
 // 函数重载：同一个函数名，但是入参列表不同
 
 // const_cast 可以用来移除(或添加) 引用或者指针的const限定符
-const string &shorterString(const string &s1, const string &s2)
-{
-  return s1.size() < s2.size() ? s1 : s2;
-}
+// const string &shorterString(const string &s1, const string &s2)
+// {
+//   return s1.size() < s2.size() ? s1 : s2;
+// }
 
-string &shorterString2(string &s1, string &s2)
-{
-  auto &r = shorterString(const_cast<const string&>(s1), const_cast<const string&>(s2));
+// string &shorterString2(string &s1, string &s2)
+// {
+//   auto &r = shorterString(const_cast<const string&>(s1), const_cast<const string&>(s2));
 
-  return const_cast<string&>(r);
-}
+//   return const_cast<string&>(r);
+// }
+
+// int main()
+// {
+//   string s1 = "aaa";
+//   const string s2 = "bbbb";
+
+//   // const_cast<const string&>(s1) = "ad";
+
+//   // 我们也可以用显式转换运算符，但是它不会检查类型
+//   // 也就是下面的代码，即使我把一个string转换成int都行，编译器也不会报错
+//   // 因此用const_cast要更安全
+//   auto &ret = (string &)shorterString(s1, s2);
+
+//   // error
+//   ret = "cc";
+
+//   std::cout << "s1: " << s1 << std::endl;
+// }
 
 int main()
 {
-  string s1 = "aaa";
-  const string s2 = "bbbb";
+  const int constant = 20;
 
-  // const_cast<const string&>(s1) = "ad";
+  const int *p = &constant;
 
-  // 我们也可以用显式转换运算符，但是它不会检查类型
-  // 也就是下面的代码，即使我把一个string转换成int都行，编译器也不会报错
-  // 因此用const_cast要更安全
-  auto &ret = (string &)shorterString(s1, s2);
+  int *ret = const_cast<int *>(p);
 
-  // error
-  ret = "cc";
+  *ret = 10;
 
-  std::cout << "s1: " << s1 << std::endl;
+  std::cout << "constant: " << constant << std::endl;
+  std::cout << "ret: " << *ret << std::endl;
+  std::cout << "&constant: " << &constant << std::endl;
+  std::cout << "&ret: " << ret << std::endl;
 }
