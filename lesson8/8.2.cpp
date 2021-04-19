c8a7192d2217058a9a823654c7661fdcb01406d0
soyal <xiongxiao9958@gmail.com>
2021-04-19 17:04:27 +0800

    8.2 complete

--- lesson8/8.2.cpp
+++ lesson8/8.2.cpp
@@ -5,28 +5,32 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <filesystem>
 
 using namespace std;
-namespace fs = std::filesystem;
 
 int main()
 {
   // 这里有个问题：怎么处理文件路径，类似__dirname
-  string fileName("test.txt");
+  string fileName("./assets/test.txt");
   ifstream in;
+  ofstream out;
 
   in.open(fileName);
+  // 默认是trunc模式，即会覆盖原来的内容
+  // out.open("./temp.txt");
+  // app模式即追加模式
+  out.open("./8.2.temp.txt", ofstream::app);
 
-  cout << fs.get_current() << endl;
-
-  if(in) {
-    cout << "ok1" << endl;
+  if (in)
+  {
     string temp;
-    while(getline(in, temp)) {
-      cout << temp << endl;
+    while (getline(in, temp))
+    {
+      out << temp << endl;
     }
-  } else {
+  }
+  else
+  {
     cout << in.failbit << endl;
   }
 }
